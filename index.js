433dbef0ee19dd47f997cea7e87d47d047f2d6f1
Zeo <zmcsoltai@gmail.com>
2025-07-26 16:13:11 +0200

    add more port stuff just for render

--- index.js
+++ index.js
@@ -16,7 +16,18 @@ const {
   EmbedBuilder,
 } = require("discord.js");
 
-const port = process.env.PORT || 4000;
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 // hi mom!
 
 const EPHEMERAL = 1 << 6;
